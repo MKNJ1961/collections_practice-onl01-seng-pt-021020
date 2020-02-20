443ebbb3e0783d5a66e07e25a671572a45f48508
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-20 10:36:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,10 +16,10 @@ require 'pry'
   end
 end
 
-    def swap_elements(array)
-        array[1], array[2] = array[2], array[1]
-      end
-  end
+  #   def swap_elements(array)
+  #       array[1], array[2] = array[2], array[1]
+  #     end
+  # end
   
 
   
