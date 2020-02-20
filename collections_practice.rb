59e76365ad6236e4d96f2e3b1cb7f251b5078d11
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-20 03:34:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,7 +13,7 @@
   end
   
   def swap_elements(array)
-    array.sort do |a, b | array[1], array[2] =array[3], array[1]
+    array.sort do |a, b | array[1], array[2] = array[3], array[1]
   end
   
 
