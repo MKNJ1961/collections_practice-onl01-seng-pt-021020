dc3eac5f7d89d2bfc9ca7567d7b97ec4fc05933f
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-20 10:32:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
   
   def sort_array_asc(array)
     array.sort { |a, b| a <=> b }
@@ -8,7 +8,7 @@
   def sort_array_desc(array)
     array.sort { |a, b| b <=> a }
   end
-  
+  binding.pry
   def sort_array_char_count(array)
     array.sort do |a, b| 
       a.length <=> b.length
